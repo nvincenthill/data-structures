fa062c6602caecb3d7d42107457fce4a55c25145
Hack Reactor Student <nvincenthill@gmail.com>
2018-06-13 13:26:16 -0700

    Implement psuedoclassical solution

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
+  
 };
 
+Queue.prototype.enqueue = function(value) {
+  for (let i = 0; i < this.length; i++) {
+    this.storage[i + 1] = this.storage[i]
+  }
+  this.length ++;
+  this.storage[0] = value;
+  return value;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.length > 0) {
+    let dequeued = this.storage[this.length - 1];
+    delete this.storage[this.length];
+    this.length --;
+    return dequeued;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.key = 0;
+  
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.key] = value;
+  this.key ++;
+  return value;
+};
 
+Stack.prototype.pop = function() {
+  if (this.key > 0) {
+    let popped = this.storage[this.key - 1];
+    delete this.storage[this.key];
+    this.key --;
+    return popped;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.key;
+};
