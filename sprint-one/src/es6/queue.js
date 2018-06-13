71f6f2f393a5e141c943b7823330cf12b2de2c5d
Hack Reactor Student <nvincenthill@gmail.com>
2018-06-13 14:52:50 -0700

    Implement es6 solutions

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+      this.storage = {};
+      this.length = 0;
+  };
 
+  enqueue(value) {
+    console.log(this);
+    for (let i = 0; i < this.length; i++) {
+      this.storage[i + 1] = this.storage[i]
+    }
+    this.length ++;
+    this.storage[0] = value;
+    return value;
+  };
+  
+  dequeue() {
+    if (this.length > 0) {
+      let dequeued = this.storage[this.length - 1];
+      delete this.storage[this.length];
+      this.length --;
+      return dequeued;
+    }
+  };
+  
+  size() {
+    return this.length;
+  };
+  
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+      this.storage = {};
+      this.key = 0;
+  };
 
+  push(value) {
+    this.storage[this.key] = value;
+    this.key ++;
+    return value;
+  };
+  
+  pop() {
+    if (this.key > 0) {
+      let popped = this.storage[this.key - 1];
+      delete this.storage[this.key];
+      this.key --;
+      return popped;
+    }
+  };
+  
+  size() {
+    return this.key;
+  };
+  
 }
