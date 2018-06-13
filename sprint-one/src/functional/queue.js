9ddd5fed5683c481537fdf0adf5a8af096722c56
Hack Reactor Student <nvincenthill@gmail.com>
2018-06-13 12:03:46 -0700

    Implement functional stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
-var Queue = function() {
-  var someInstance = {};
+let Queue = function() {
+  let someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  let storage = {};
+  let length = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    for (let i = 0; i < length; i++) {
+      storage[i + 1] = storage[i]
+    }
+    length ++;
+    storage[0] = value;
+    return value;
   };
 
   someInstance.dequeue = function() {
+    if (length > 0) {
+      let dequeued = storage[length - 1];
+      delete storage[length];
+      length --;
+      return dequeued;
+    }
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
