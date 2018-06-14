0cec2793e42dd0559b1df5b7c697886ed7269474
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-06-14 10:36:03 -0700

    Implement Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,13 +1,15 @@
 // Specification:
 
-// Inputs
-// Outputs
-// Constraints
-// Edge Cases
+// Inputs - none
+// Outputs - object
+// Constraints - time complexity?
+// Edge Cases - ???
   
-// Justification - 
+// Justification - To create a tree data structure 
 
-// Explanation -
+// Explanation - A tree is a hierarchical data structure consisting of a node (potentially) with children.
+// The children are trees unto themselves, that is, nodes with (potential) children.
+// For this reason the tree is referred to as a recursive data structure.
 
 // *** WHITE BOARD FIRST *** 
 
@@ -15,23 +17,39 @@
 
 // *** WHEN ALL ABOVE ARE FULLY COMPLETE THEN TYPE SOME CODE ***
 
+// create a tree
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  Object.assign(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
 
-var treeMethods = {};
+var treeMethods = {
+  
+  // add a tree to of children of a tree - O(1) time complexity
+  addChild: function(value) {
+    var child = Tree(value);
+    this.children.push(child);
+  },
 
-treeMethods.addChild = function(value) {
+  // check if a value is contained in the tree - O(n) time complexity
+  contains: function(target) {
+    if (target === this.value) {
+      return true;
+    }
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 
-treeMethods.contains = function(target) {
-};
 
 
 
