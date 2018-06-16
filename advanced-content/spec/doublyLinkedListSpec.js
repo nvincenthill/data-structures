aec83569d8d1ffd2a0df2aea8dfa69925d630aad
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-06-16 09:53:09 -0700

    Implement DoublyLinkedList

--- advanced-content/spec/doublyLinkedListSpec.js
+++ advanced-content/spec/doublyLinkedListSpec.js
@@ -3,18 +3,97 @@ describe('doublyLinkedList', function() {
   let doublyLinkedList;
   
   beforeEach(function() {
-    // doublyLinkedList = new doublyLinkedList();
+    doublyLinkedList = DoublyLinkedList();
   });
 
-  it('test test', function() {
-    expect(true).to.be.a('boolean');
+  it('should have a head and tail', function() {
+    expect(doublyLinkedList).to.have.property('head');
+    expect(doublyLinkedList).to.have.property('tail');
+  });
 
+  it('should have methods named "addToTail", "removeHead", "contains", "addToHead", and "removeTail', function() {
+    expect(doublyLinkedList.addToTail).to.be.a('function');
+    expect(doublyLinkedList.removeHead).to.be.a('function');
+    expect(doublyLinkedList.contains).to.be.a('function');
+  });
+
+  it('should designate a new tail when new nodes are added to tail', function() {
+    doublyLinkedList.addToTail(4);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    doublyLinkedList.addToTail(5);
+    expect(doublyLinkedList.tail.value).to.equal(5);
+  });
+
+  it('should remove the head from the list when removeHead is called', function() {
+    doublyLinkedList.addToTail(4);
+    doublyLinkedList.addToTail(5);
+    expect(doublyLinkedList.head.value).to.equal(4);
+    doublyLinkedList.removeHead();
+    expect(doublyLinkedList.head.value).to.equal(5);
+  });
+
+  it('should return the value of the former head when removeHead is called', function() {
+    doublyLinkedList.addToTail(4);
+    expect(doublyLinkedList.removeHead()).to.equal(4);
+  });
+
+  it('should contain a value that was added to tail', function() {
+    doublyLinkedList.addToTail(4);
+    doublyLinkedList.addToTail(5);
+    expect(doublyLinkedList.contains(4)).to.equal(true);
+    expect(doublyLinkedList.contains(5)).to.equal(true);
+    expect(doublyLinkedList.contains(6)).to.equal(false);
+  });
+
+  it('should not contain a value that was removed from head', function() {
+    doublyLinkedList.addToTail(4);
+    doublyLinkedList.addToTail(5);
+    doublyLinkedList.removeHead();
+    expect(doublyLinkedList.contains(4)).to.equal(false);
   });
   
-  // // check if an item 
-  // it('should contain an item that has been added', function() {
-  //   bloomFilter.addItem('test');
-  //   expect(bloomFilter.query('test')).to.equal(true);
-  // });
+  it('should designate a new head when new nodes are added to head', function() {
+    doublyLinkedList.addToHead(4);
+    expect(doublyLinkedList.head.value).to.equal(4);
+    doublyLinkedList.addToHead(5);
+    expect(doublyLinkedList.head.value).to.equal(5);
+  });
+
+  it('should remove the tail from the list when removeTail is called', function() {
+    doublyLinkedList.addToHead(4);
+    doublyLinkedList.addToHead(5);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    doublyLinkedList.removeTail();
+    expect(doublyLinkedList.tail.value).to.equal(5);
+  });
+
+  it('should return the value of the former tail when removeTail is called', function() {
+    doublyLinkedList.addToHead(4);
+    expect(doublyLinkedList.removeTail()).to.equal(4);
+  });
   
+  it('should contain a value that was added to head', function() {
+    doublyLinkedList.addToHead(4);
+    doublyLinkedList.addToHead(5);
+    expect(doublyLinkedList.contains(4)).to.equal(true);
+    expect(doublyLinkedList.contains(5)).to.equal(true);
+    expect(doublyLinkedList.contains(6)).to.equal(false);
+  });
+
+  it('should not contain a value that was removed from tail', function() {
+    doublyLinkedList.addToHead(4);
+    doublyLinkedList.addToHead(5);
+    doublyLinkedList.removeTail();
+    expect(doublyLinkedList.contains(4)).to.equal(false);
+  });
+  
+  it('should have null head and tail after last node is removed', function() {
+    doublyLinkedList.addToTail(4);
+    doublyLinkedList.addToTail(5);
+    doublyLinkedList.removeHead();
+    doublyLinkedList.removeHead();
+    expect(doublyLinkedList.head).to.equal(null);
+    expect(doublyLinkedList.tail).to.equal(null);
+  });
+    
 });
--- advanced-content/src/doublyLinkedList.js
+++ advanced-content/src/doublyLinkedList.js
@@ -1,13 +1,13 @@
 // Specification:
 
-// Inputs - m, k as positive integers where m > k
+// Inputs - none
 // Outputs - object
 // Constraints - time complexity?
 // Edge Cases - ???
   
-// Justification - To create a Bloom filter.
+// Justification - To create a linked list data structure 
 
-// Explanation - A Bloom filter is a space-efficient probabilistic data structure
+// Explanation - Data structure storing a sequence of elements. Each element is a node that contains a data value and a reference to the next element.
 
 // *** WHITE BOARD FIRST *** 
 
@@ -15,28 +15,86 @@
 
 // *** WHEN ALL ABOVE ARE FULLY COMPLETE THEN TYPE SOME CODE ***
 
-// Instantiate a bloom filter
-// var DoublyLinkedList = function(m, k) {
-//   this.bits = new Array(m);
-//   this.hashes = generateHashFunctions(k, m);
-// };
+// create a linked list
+var DoublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
 
-// // ???  - O(?) time complexity
-// BloomFilter.prototype.addItem = function(item) {
-//   for (var i = 0; i < this.hashes.length; i++) {
-//     this.bits[this.hashes[i](JSON.stringify(item))] = 1;
-//   }
-// };
+  // add a node to head of list - O(1) time complexity
+  list.addToHead = function(value) {
+    let node = Node(value);
+    if (!list.head) {
+      list.tail = node;
+    } else {
+      list.head.prev = node;
+      node.next = list.head;
+    }
+    list.head = node;
+  };
 
-// // ???  - O(?) time complexity
-// BloomFilter.prototype.query = function(item) {
-//   for (var i = 0; i < this.hashes.length; i++) {
-//     if (!this.bits[this.hashes[i](JSON.stringify(item))]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+  // add a node to tail of list - O(1) time complexity
+  list.addToTail = function(value) {
+    let node = Node(value);
+    if (!list.head) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+      node.prev = list.tail;
+    }
+    list.tail = node;
+  };
 
+  // remove the node at the head - O(1) time complexity
+  list.removeHead = function() {
+    let formerHead = list.head.value;
+    if (list.head) {
+      list.head = list.head.next;
+      if (!list.head) {
+        list.tail = null;
+      } else {
+        list.head.prev = null;        
+      }
+    }   
+    return formerHead;
+  };
+  
+  // remove the node at the tail - O(1) time complexity
+  list.removeTail = function() {
+    let formerTail = list.tail.value;
+    if (list.tail) {
+      list.tail = list.tail.prev;
+      if (!list.tail) {
+        list.head = null;
+      } else {
+        list.tail.next = null;
+      }
+    } 
+    return formerTail;
+  };
 
+  // check to see if value is contained in list - O(n) time complexity
+  list.contains = function(target) {
+    let currentNode = list.head;
+    while (currentNode !== null) {
+      if (target === currentNode.value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  };
+  
+  return list;
+};
 
+// create a node
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.prev = null;
+
+  return node;
+};
